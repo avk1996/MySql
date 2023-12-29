7b545141df548cd34bef60134388ab804841ea68
Abhishek Kumbhar <62824193+avk1996@users.noreply.github.com>
2023-12-29 23:57:52 +0530

    starting with triggers

---
+++ Triggers/triggers.sql
@@ -0,0 +1,12 @@
+-- Executed automaticall on some events
+-- Events like INSERT, UPDATE AND DELETE
+-- Triggers are executed in DML opeartions
+-- Before and After DMLs
+-- No parameters and no return value
+-- Triggers are supported by standard RDBMS
+
+-- Triggers can contain SQL statements and programming constructs
+-- Triggers may call SF or SP
+
+-- COMMIT/ROLLBACK are NOT ALLOWED in Triggers
+
