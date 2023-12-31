60a9b131fea134c0a4e5bd669ce769c0468a4782
Abhishek Kumbhar <62824193+avk1996@users.noreply.github.com>
2023-12-31 15:20:24 +0530

    cursor work in progress

--- LabExamPractice/first_name_list.sql
+++ LabExamPractice/first_name_list.sql
@@ -8,11 +8,16 @@ BEGIN
 
 DECLARE v_flag INT DEFAULT 0;
 DECLARE v_name_worker VARCHAR(255) DEFAULT "";
+DECLARE worker_list VARCHAR(255);
+DECLARE temp VARCHAR(30);
 DECLARE v_cur CURSOR FOR SELECT FIRST_NAME FROM worker;
+
 DECLARE CONTINUE HANDLER FOR NOT FOUND SET v_flag=1;
 
   OPEN v_cur;
-  
+
+  SET @worker_list = NULL;  
+    
     label:LOOP
   
     FETCH v_cur into v_name_worker;
@@ -20,14 +25,16 @@ DECLARE CONTINUE HANDLER FOR NOT FOUND SET v_flag=1;
       IF v_flag=1 THEN
         LEAVE label;
       END IF;
-  
-    SET v_name_worker=CONCAT(v_name_worker,",");
-  
+
+    SET @temp = v_name_worker;
+    SET @worker_list = CONCAT(temp,',');
+    -- SELECT v_name_worker; 
+    
     END LOOP;
   
   CLOSE v_cur;
-  SET v_name_worker = TRIM(TRAILING ', ' FROM v_name_worker);
-  INSERT INTO result VALUES(v_name_worker);
+  INSERT INTO result VALUES(worker_list);
+  -- SET v_name_worker = TRIM(TRAILING ', ' FROM v_name_worker);
 END;
 
 $$
