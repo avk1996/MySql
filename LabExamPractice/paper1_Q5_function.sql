fedb05f8a2e6f4cb116b4afcaa173421d099a8d5
Abhishek Kumbhar <62824193+avk1996@users.noreply.github.com>
2024-01-01 13:05:23 +0530

    solved more questions

--- LabExamPractice/paper1_Q5_function.sql
+++ LabExamPractice/paper1_Q5_function.sql
@@ -1,2 +1,23 @@
--- 04. Create insert,update, delete trigger on emp table, trigger will get fire when update operation is perform on salary column which will show salary diffences old and new values.
+-- Q3. Create a pl-sql function which will accept the department number as input and it will return total salary for that department
 
+DROP FUNCTION IF EXISTS SalSum;
+
+DELIMITER &&
+
+CREATE FUNCTION SalSum(id INT)
+
+RETURNS INT
+DETERMINISTIC
+
+BEGIN
+    DECLARE v_total_sal INT;
+    SELECT SUM(e.sal) INTO v_total_sal 
+    FROM emp e INNER JOIN dept d
+    ON e.deptno = d.deptno
+    WHERE d.dname = (SELECT dname FROM dept WHERE deptno = id);
+    return v_total_sal;
+END;
+
+&&
+
+DELIMITER ; 
--- LabExamPractice/paper1_Q6_trigger.sql
+++ LabExamPractice/paper1_Q6_trigger.sql
@@ -1,23 +1,22 @@
--- Q3. Create a pl-sql function which will accept the department number as input and it will return total salary for that department
+-- 04. Create insert,update, delete trigger on emp table, trigger will get fire when update operation is perform on salary column which will show salary diffences old and new values.
 
-DROP FUNCTION IF EXISTS SalSum;
+DROP TABLE IF EXISTS SalLog;
+
+CREATE TABLE SalLog (new_sal DECIMAL(8,2),old_sal DECIMAL(8,2), sal_diff DECIMAL(8,2));
+
+DROP TRIGGER IF EXISTS sal_logs;
 
 DELIMITER &&
 
-CREATE FUNCTION SalSum(id INT)
+CREATE TRIGGER sal_logs 
 
-RETURNS INT
-DETERMINISTIC
+BEFORE INSERT ON emp
 
+FOR EACH ROW
 BEGIN
-    DECLARE v_total_sal INT;
-    SELECT SUM(e.sal) INTO v_total_sal 
-    FROM emp e INNER JOIN dept d
-    ON e.deptno = d.deptno
-    WHERE d.dname = (SELECT dname FROM dept WHERE deptno = id);
-    return v_total_sal;
+    INSERT INTO SalLog VALUES(NEW.sal,OLD.sal,ABS(NEW.sal-OLD.sal));
 END;
 
 &&
 
-DELIMITER ; 
+DELIMITER ;
